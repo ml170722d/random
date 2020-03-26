e23acb24b054009e9684cd1ee70e263c2f723a5f
Aleksa Zivkovic <zaleks710@gmail.com>
2020-03-26 15:48:14 +0100

    UI-design,verzion_v1.0

---
+++ UI-Design/login_register/login_register.js
@@ -0,0 +1,26 @@
+function reg(){
+    document.getElementById("register").style.display = "inline";
+    document.getElementById("login").style.display = "none";
+}
+
+function login(){
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style.display = "inline";
+}
+
+function init(){
+    login();
+}
+
+function signUp(formData){
+    
+    console.log("cao");
+}
+
+function signIn(){
+    console.log("hello");
+}
+
+function check(){
+
+}
